5d3956b57b7bcec6a7698e65d48b9f4a25d88441
jwillems202415 <j.willems@students.ephec.be>
2024-11-17 20:50:52 +0100

    Prototype de "super Simon"
    Ajout d'un prototype de Super Simon. Ne fonctionne pas encore !
    Il est capable de générer une séquence et la montrer mais ne sait pas encore capturer la réponse de l'utilisateur pour la vérifier.

---
+++ SuperSimon.py
@@ -0,0 +1,86 @@
+import tkinter as tk
+import random
+import time
+import threading
+
+# Main window creation.
+root = tk.Tk() # Opens a window
+root.title("Super Simon") # Titles the window
+squareImage = tk.PhotoImage(width = 1, height = 1) # This fake square image will be put into the buttons to make them square.
+
+# Buttons
+## Buttons initialisation
+greenButton = tk.Button(root, bg = 'black', image = squareImage, width = 300, height = 300, command =)
+redButton = tk.Button(root, bg = 'black', image = squareImage, width = 300, height = 300, command =)
+yellowButton = tk.Button(root, bg = 'black', image = squareImage, width = 300, height = 300, command =)
+blueButton = tk.Button(root, bg = 'black', image = squareImage, width = 300, height = 300, command =)
+
+## Position the various buttons in a grid
+greenButton.grid(row = 0, column = 0) # Top left
+redButton.grid(row = 0, column = 1) # Top right
+yellowButton.grid(row = 1, column = 0) # Bottom left
+blueButton.grid(row = 1, column = 1) # Botton right
+
+# Sequence
+sequence = []
+
+def addUnitToSequence(): # Adds a unit to the sequence
+    buttonsList = ['Green', 'Red', 'Yellow', 'Blue']
+    sequence.append( random.choice(buttonsList) )
+    print(sequence)
+
+def showSequenceOnGUI(): # Shows the sequence on the GUI
+    for unit in sequence:
+        if unit == 'Green':
+            greenButton.config(bg = 'green') # Green button get turned on.
+            time.sleep(1) # Wait 1 second.
+            greenButton.config(bg = 'black') # Turn green button off.
+        elif unit == 'Red':
+            redButton.config(bg = 'red')
+            time.sleep(1)
+            redButton.config(bg = 'black')
+        elif unit == 'Yellow':
+            yellowButton.config(bg = 'yellow')
+            time.sleep(1)
+            yellowButton.config(bg = 'black')
+        elif unit == 'Blue':
+            blueButton.config(bg = 'blue')
+            time.sleep(1)
+            blueButton.config(bg = 'black')
+
+def userInputing(): # Lets the user input.
+    inputSequence = []
+    def sequenceComparator(): # Compares the user's entered sequence with the global sequence.
+        if inputSequence == sequence:
+            return True
+        else:
+            return False
+    
+    # Corps
+    # Attente de l'input d'un utilisateur
+    waitForClick = tk.IntVar() # Waits for a user to click a button on the GUI
+    
+    
+    
+
+def main():
+    '''Programme principal'''
+    run = 0
+    while run < 10:
+        addUnitToSequence()
+        showSequenceOnGUI()
+        run += 1
+
+
+# Starts the main program
+thread = threading.Thread(target = main)
+thread.start()
+
+root.mainloop()
+
+
+
+"""
+sequenceLength = input("Enter the length of the sequence:\n> ")
+print(f"Entered length = {sequenceLength}\n")
+"""
